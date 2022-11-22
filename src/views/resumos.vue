13d14821512b66fa5e79bf816cfc2700f95bbcd5
mayconrcampos <maycon.campos@gmail.com>
2022-11-21 22:46:22 -0300

    C03 feats: evoluindo aplicativo, criando tables, melhorando css, hovers, transitions, entre outros

--- src/views/resumos.vue
+++ src/views/resumos.vue
@@ -1,15 +1,50 @@
 <template>
-    <div class="container">
-        <h3>Resumo Financeiro</h3>
+  <div class="container">
+    <h3>Resumo Financeiro</h3>
+    <div class="table-responsive">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Descrição</th>
+            <th scope="col">Valor (R$)</th>
+            <th scope="col">Data</th>
+            <th scope="col">Categoria</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Santos Supermercados</th>
+            <td>59,90</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">Padaria</th>
+            <td>77,50</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">Vestecasa</th>
+            <td>22,90</td>
+            <td>Pau no cú do twitter</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <th>Saldo</th>
+            <th>R$ Muita grana</th>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "resuMos"
-}
+  name: "resuMos",
+};
 </script>
 
 <style scoped>
-
 </style>
